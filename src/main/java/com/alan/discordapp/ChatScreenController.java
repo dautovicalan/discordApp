1d8b90d3d41c191cbd0dd942e09c457d0313887d
dautovicalan <dautovic2000@gmail.com>
2022-10-15 15:01:24 +0200

    new chat design and server class

--- src/main/java/com/alan/discordapp/ChatScreenController.java
+++ src/main/java/com/alan/discordapp/ChatScreenController.java
@@ -1,13 +1,25 @@
 package com.alan.discordapp;
 
+import com.alan.models.Server;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -15,21 +27,85 @@ import java.util.ResourceBundle;
 public class ChatScreenController implements Initializable {
 
     @FXML
-    private TextArea chatBox;
-    @FXML
     private TextField textField;
     @FXML
     private Button sendButton;
+    @FXML
+    private VBox vBoxMessage;
+    @FXML
+    private ScrollPane scrollPaneMain;
+
+    private Server server;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    }
-
-    public void sendMessage(){
-        if (textField.getText().equals("")){
-            return;
+        try {
+            server = new Server(new ServerSocket(1234));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error creating server");
         }
 
-        chatBox.appendText(textField.getText() + "\n");
-        textField.clear();
+        vBoxMessage.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number newValue) {
+                scrollPaneMain.setVvalue((Double) newValue);
+            }
+        });
+
+        server.receiveMessageFromClient(vBoxMessage);
+
+        sendButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String messageToSend = textField.getText();
+                if (!messageToSend.isEmpty()){
+                    HBox hBox = new HBox();
+                    hBox.setAlignment(Pos.CENTER_RIGHT);
+                    hBox.setPadding(new Insets(5,5,5,10));
+
+                    Text text = new Text(messageToSend);
+                    TextFlow textFlow = new TextFlow(text);
+
+                    textFlow.setStyle("-fx-color: rgb(239, 242, 255); " +
+                            "-fx-background-color: rgb(15,125,242); " +
+                            "-fx-background-radius: 20px;");
+
+                    textFlow.setPadding(new Insets(5,5,5,10));
+                    text.setFill(Color.color(0.934, 0.945, 0.996));
+
+                    hBox.getChildren().add(textFlow);
+                    vBoxMessage.getChildren().add(hBox);
+
+                    server.sendMessageToClient(messageToSend);
+                    textField.clear();
+                }
+            }
+        });
     }
+
+    public static void addMessageToComponent(String messageFromClient, VBox vBox){
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5,5,5,10));
+
+        Text text = new Text(messageFromClient);
+        TextFlow textFlow = new TextFlow(text);
+
+        textFlow.setStyle("-fx-background-color: rgb(233,233,235); " +
+                "-fx-background-radius: 20px;");
+
+        textFlow.setPadding(new Insets(5,5,5,10));
+        text.setFill(Color.color(0.934, 0.945, 0.996));
+
+        hBox.getChildren().add(textFlow);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                vBox.getChildren().add(hBox);
+            }
+        });
+
+    }
+
 }
--- src/main/java/com/alan/models/Server.java
+++ src/main/java/com/alan/models/Server.java
@@ -1,9 +1,78 @@
 package com.alan.models;
 
+import com.alan.discordapp.ChatScreenController;
+import javafx.scene.layout.VBox;
+
+import java.io.*;
 import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-    private int port;
-    private Inet4Address ipAddress;
 
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public Server(ServerSocket serverSocket) {
+
+        try {
+            this.serverSocket = serverSocket;
+            this.socket = serverSocket.accept();
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e){
+            System.out.println("Error creating server");
+            e.printStackTrace();
+            closeEveryConnection(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void sendMessageToClient(String message){
+        try {
+            bufferedWriter.write(message);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error sending message to client!");
+            closeEveryConnection(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void receiveMessageFromClient(VBox vBox){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (socket.isConnected()){
+                    try {
+                        String messageFromClient = bufferedReader.readLine();
+                        ChatScreenController.addMessageToComponent(messageFromClient, vBox);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("Error receivieng message from client");
+                        closeEveryConnection(socket, bufferedReader, bufferedWriter);
+                        break;
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeEveryConnection(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try {
+            if (bufferedReader != null){
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null){
+                bufferedReader.close();
+            }
+            if (socket != null){
+                socket.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
